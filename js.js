c127fade53497367fd4efdad7fb3010367090528
SchmiedmeiszterAdam <smikuadam@gmail.com>
2021-10-28 00:48:51 +0200

    idő javítások, pulzáló animáció

--- js.js
+++ js.js
@@ -1,25 +1,38 @@
 $(function () {
-    setInterval(idoMeghatarozas, 100)
+    idoMeghatarozas()
+    setInterval(idoMeghatarozas, 1000)
     function idoMeghatarozas() {
+        $("#masodperc").removeClass('pulzalas')
+        setTimeout(function(){$("#masodperc").addClass('pulzalas')},10)
         const datum = new Date()
         const egyNap = 1000 * 60 * 60 * 24;
         let ev = datum.getFullYear()
         if (datum.getMonth() == 11 && datum.getDate() > 25) {
             ev = ev + 1;
         }
-        const karacsony = new Date(ev, 11, 25)
+        const karacsony = new Date(ev, 11, 24)
         let kul = karacsony - datum
         var kari = Math.floor(kul / egyNap)
-
-        let ora = 23 - datum.getHours()
-        let perc = 60 - datum.getMinutes()
         
-
+        let ora = 23 - datum.getHours()
+        let perc = 59 - datum.getMinutes()
+        
         let masdoperc = 59 - datum.getSeconds()
+        if(masdoperc === 0){
+            $("#perc").addClass('pulzalas')
+            setTimeout(function(){$("#perc").removeClass('pulzalas')},1000)
+        }
+        if(perc === 0){
+            $("#ora").addClass('pulzalas')
+            setTimeout(function(){$("#ora").removeClass('pulzalas')},1000)
+        }
+        if(ora === 0){
+            $("#nap").addClass('pulzalas')
+            setTimeout(function(){$("#nap").removeClass('pulzalas')},1000)
+        }
         if (masdoperc < 10) {
             masdoperc = '0' + masdoperc
         }
-
         idoKiiras()
         function idoKiiras() {
             $("#nap").html(kari)
